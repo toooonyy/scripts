e9a66c31a5921c4f139bbfa6a74f82c42be2feb7
TonyY <wsjswps@gmail.com>
2015-08-19 16:53:27 +0800

    First commit

---
+++ mpv/scripts/vo_battery.lua
@@ -0,0 +1,15 @@
+-- If the laptop is on battery, the VO set in the config will be choosen,
+-- else the one defined with "hqvo" is used.
+local hqvo = "opengl-hq:interpolation:swapinterval=1:scaler-resizes-only:scale=spline36:scale-radius=4:scale-antiring=1.0:cscale=spline36:cscale-radius=3:dscale=spline36:dscale-radius=3:tscale=oversample:dither-depth=8:fbo-format=rgb32f:linear-scaling:icc-profile-auto"
+
+local utils = require 'mp.utils'
+if mp.get_property_bool("option-info/vo/set-from-commandline") == true then
+    return
+end
+t = {}
+t.args = {"/usr/bin/pmset", "-g", "ac"}
+res = utils.subprocess(t)
+if res.stdout ~= "No adapter attached.\n" then
+    mp.set_property("options/vo",hqvo)
+    mp.set_property_number("display-fps",60)
+end
