ef15011dbecb08a4686f86962234fd201045a745
TonyY <wsjswps@gmail.com>
2015-08-24 23:59:12 +0800

    a script that recodes files from 'gbk' to 'utf8' usage ./convert.py src dest

---
+++ python/convert.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -* - coding: UTF-8 -* -
+
+import os,re,sys,shutil
+
+def search(src,dest,handle):
+    filelist=os.listdir(src)
+    for f in filelist:
+        cf=src+'/'+f
+        df=dest+'/'+f
+        handle(cf,df)
+
+def convert(file_in,file_out):
+    fi=open(file_in,'r')
+    content=fi.read()
+    try:
+        content=content.decode('gbk')
+        fo=open(file_out,'w')
+        fo.write(content.encode('utf-8'))
+        fo.flush()
+        fo.close()
+        fi.close()
+
+    except:
+        fi.close()
+        sys.exit()
+
+
+if __name__ == '__main__':
+    srcDir=os.getcwd()+'/'+sys.argv[1]
+    dstDir=os.getcwd()+'/'+sys.argv[2]
+search(srcDir,dstDir,convert)
