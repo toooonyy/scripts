87e584125546bdb8ffe3019a72b044dc65d8876d
TonyY <wsjswps@gmail.com>
2015-08-28 14:19:43 +0800

    rm mpv

--- mpv/scripts/vo_battery.lua
+++ mpv/scripts/vo_battery.lua
@@ -1,6 +1,6 @@
 -- If the laptop is on battery, the VO set in the config will be choosen,
 -- else the one defined with "hqvo" is used.
-local hqvo = "opengl-hq:interpolation:swapinterval=1:scaler-resizes-only:scale=spline36:scale-radius=4:scale-antiring=1.0:cscale=spline36:cscale-radius=3:dscale=spline36:dscale-radius=3:tscale=oversample:dither-depth=8:fbo-format=rgb32f:linear-scaling:icc-profile-auto"
+local hqvo = "opengl-hq:interpolation:swapinterval=1:scaler-resizes-only:scale=spline36:scale-radius=3:scale-antiring=1.0:dscale=mitchell:cscale=spline36:cscale-radius=3:tscale=oversample:dither-depth=8:fbo-format=rgb16f:linear-scaling:icc-profile-auto"
 
 local utils = require 'mp.utils'
 if mp.get_property_bool("option-info/vo/set-from-commandline") == true then
