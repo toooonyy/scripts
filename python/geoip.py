2483a8aa0f660f54759bf5f0f6c6ab8566beb9b8
TonyY <wsjswps@gmail.com>
2016-07-17 15:10:09 +0800

    New geoip script in python

---
+++ python/geoip.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -* - coding: UTF-8 -* -
+
+import requests
+from subprocess import call
+import sys
+from bs4 import BeautifulSoup
+import itertools
+
+call("sudo mtr -rn " + sys.argv[1] + """ -c 3 | awk '$2 ~ "[12]?[0-9]?[0-9]\.[12]?[0-9]?[0-9]\.[12]?[0-9]?[0-9]\.[12]?[0-9]?[0-9]"{print $2}' > /tmp/geoip""", shell=True)
+
+def get_ip_from_ipcn(ip):
+    r = requests.get('http://ip.cn/index.php?ip=' + ip)
+    soup = BeautifulSoup(r.text, "html.parser")
+    address = [x.string for x in soup.findAll("code")][1]
+    return address
+
+def get_ip_from_ipipnet(ip):
+    r = requests.get('http://freeapi.ipip.net/' + ip)
+    address = ' '.join(x for x in filter(None, eval(r.text)))
+    return address
+
+with open('/tmp/geoip','r') as f:
+    ip_address = f.read().splitlines()
+
+table_list = [[x, get_ip_from_ipcn(x), get_ip_from_ipipnet(x)] for x in ip_address]
+max_wide = max([len(x) for x in list(itertools.chain(*table_list))]) + 1
+
+for row in table_list:
+    print ("{:<{width}}{:<{width}}{:<{width}}".format(*row, width = max_wide))
