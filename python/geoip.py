e84332092f8126ea2b16ae35134fd95c60e2a437
TonyY <wsjswps@gmail.com>
2016-07-18 16:20:32 +0800

    Update code style

--- python/geoip.py
+++ python/geoip.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 # -* - coding: UTF-8 -* -
 
-import requests
-from subprocess import call
 import sys
-from bs4 import BeautifulSoup
+from subprocess import call
 import itertools
+import requests
+from bs4 import BeautifulSoup
 
 call("sudo mtr -rn " + sys.argv[1] + """ -c 3 | awk '$2 ~ "[12]?[0-9]?[0-9]\.[12]?[0-9]?[0-9]\.[12]?[0-9]?[0-9]\.[12]?[0-9]?[0-9]"{print $2}' > /tmp/geoip""", shell=True)
+
 
 def get_ip_from_ipcn(ip):
     r = requests.get('http://ip.cn/index.php?ip=' + ip)
@@ -15,16 +16,17 @@ def get_ip_from_ipcn(ip):
     address = [x.string for x in soup.findAll("code")][1]
     return address
 
+
 def get_ip_from_ipipnet(ip):
     r = requests.get('http://freeapi.ipip.net/' + ip)
     address = ' '.join(x for x in filter(None, eval(r.text)))
     return address
 
-with open('/tmp/geoip','r') as f:
+with open('/tmp/geoip', 'r') as f:
     ip_address = f.read().splitlines()
 
 table_list = [[x, get_ip_from_ipcn(x), get_ip_from_ipipnet(x)] for x in ip_address]
 max_wide = max([len(x) for x in list(itertools.chain(*table_list))]) + 1
 
 for row in table_list:
-    print ("{:<{width}}{:<{width}}{:<{width}}".format(*row, width = max_wide))
+    print ("{:<{width}}{:<{width}}{:<{width}}".format(*row, width=max_wide))
