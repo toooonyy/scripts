eac9ef30c49cbdbf307be7c6238b4f0075a05c49
TonyY <wsjswps@gmail.com>
2016-08-01 17:37:32 +0800

    Change chroma upscale filter from spline36 to bicubic

--- mpv/scripts/vo_battery.lua
+++ mpv/scripts/vo_battery.lua
@@ -1,6 +1,6 @@
 -- If the laptop is on battery, the VO set in the config will be choosen,
 -- else the one defined with "hqvo" is used.
-local hqvo = "opengl:scale=spline36:dscale=mitchell:cscale=spline36:temporal-dither:dither-depth=8:correct-downscaling:linear-scaling:icc-profile-auto:blend-subtitles=video:pbo"
+local hqvo = "opengl:scale=spline36:dscale=mitchell:cscale=bicubic:temporal-dither:dither-depth=8:correct-downscaling:linear-scaling:icc-profile-auto:blend-subtitles=video:pbo"
 local utils = require 'mp.utils'
 if mp.get_property_bool("option-info/vo/set-from-commandline") == true then
     return
