96177da554e110566fcf8ae4cf63d45e67bca687
TonyY <wsjswps@gmail.com>
2016-09-06 10:58:55 +0800

    Update script to consist with new mpv conf

--- mpv/scripts/vo_battery.lua
+++ mpv/scripts/vo_battery.lua
@@ -1,6 +1,5 @@
 -- If the laptop is on battery, the VO set in the config will be choosen,
 -- else the one defined with "hqvo" is used.
-local hqvo = "opengl:scale=spline36:dscale=mitchell:cscale=bicubic:temporal-dither:dither-depth=8:correct-downscaling:linear-scaling:icc-profile-auto:blend-subtitles=video:pbo"
 local utils = require 'mp.utils'
 if mp.get_property_bool("option-info/vo/set-from-commandline") == true then
     return
@@ -9,5 +8,10 @@ t = {}
 t.args = {"/usr/bin/pmset", "-g", "ac"}
 res = utils.subprocess(t)
 if res.stdout ~= "No adapter attached.\n" then
-    mp.set_property("options/vo",hqvo)
+    mp.set_property("scale","spline36")
+    mp.set_property("cscale","bicubic")
+    mp.set_property("temporal-dither","yes")
+    mp.set_property("correct-downscaling","yes")
+    mp.set_property("linear-scaling","yes")
+    mp.set_property("opengl-pbo","yes")
 end
